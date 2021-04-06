b7123b7d6489442b8c749969aa75f847d8baebc8
viniciusFreitas00 <vini.freirefreitas00@gmail.com>
2021-04-05 21:29:10 -0300

    add historico e teste

--- src/app/calculadora/Components/calculadora.component.ts
+++ src/app/calculadora/Components/calculadora.component.ts
@@ -4,88 +4,100 @@ import { CalculadoraService } from '../services/calculadora.service';
 @Component({
   selector: 'app-calculadora',
   templateUrl: './calculadora.component.html',
-  styleUrls: ['./calculadora.component.css']
+  styleUrls: ['./calculadora.component.css'],
 })
 export class CalculadoraComponent implements OnInit {
-  
   private numero1: string;
   private numero2: string;
   private resultado: number;
   private operacao: string;
-  
-  constructor( private CalculadoraService: CalculadoraService ) {
-  }
+  public hist: Array<string> = [''];
 
-  ngOnInit(): void { 
+  constructor(private calculadoraService: CalculadoraService) {}
+
+  ngOnInit(): void {
     this.limpar();
-   }
-
-  limpar(): void {
-    this.numero1 = '0';
-    this.numero2 = null;
-    this.resultado = null;
-    this.operacao = null;
   }
 
-  adicionarNumero(Numero: string): void{
-    if (this.operacao === null){
-      this.numero1 = this.concatenarNumero(this.numero1, Numero);
-    }
-    else{
-      this.numero2 = this.concatenarNumero(this.numero2, Numero);
+  adicionarNumero(numero: string): void {
+    if (this.operacao === null) {
+      this.numero1 = this.concatenarNumero(this.numero1, numero);
+    } else {
+      this.numero2 = this.concatenarNumero(this.numero2, numero);
     }
   }
 
-  concatenarNumero(numAtual: string, numConcat: string): string{
-    if (numAtual === '0'|| numAtual === null){
+  concatenarNumero(numAtual: string, numConcat: string): string {
+    if (numAtual === '0' || numAtual === null) {
       numAtual = '';
     }
-    if(numConcat === '.' && numAtual === ''){
-      return '0.'
+    if (numConcat === '.' && numAtual === '') {
+      return '0.';
     }
-    if (numConcat === '.' && numAtual.indexOf('.') > -1 ){
+    if (numConcat === '.' && numAtual.indexOf('.') > -1) {
       return numAtual;
+    } else {
+      return numAtual + numConcat;
     }
-
-    return numAtual + numConcat;
   }
 
-  definirOperacao(operacao: string): void{
-    if (this.operacao === null){
+  definirOperacao(operacao: string): void {
+    if (this.operacao === null) {
       this.operacao = operacao;
+      this.hist.push(this.numero1);
+      this.hist.push(this.operacao);
       return;
     }
-    if (this.numero2 !== null){
-      this.resultado = this.CalculadoraService.calcular(
+
+    if (this.numero2 !== null) {
+      this.resultado = this.calculadoraService.calcular(
         parseFloat(this.numero1),
         parseFloat(this.numero2),
         this.operacao
-      )
-      this.operacao = operacao;
-      this.numero1 = this.resultado.toString();
-      this.numero2 = null;
-      this.resultado = null;
+      );
     }
+
+    this.operacao = operacao;
+    this.numero1 = this.resultado.toString();
+    this.numero2 = null;
+    this.resultado = null;
+    this.hist.push(this.operacao);
   }
 
-  calcular(): void{
-    if (this.numero2 === null){
-      return
+  calcular(): void {
+    if (this.numero2 === null) {
+      return;
     }
-    this.resultado = this.CalculadoraService.calcular(
+
+    this.hist.push(this.numero2);
+    this.hist.push('=');
+
+    this.resultado = this.calculadoraService.calcular(
       parseFloat(this.numero1),
       parseFloat(this.numero2),
       this.operacao
     );
   }
 
-  get display(): string{
-    if (this.resultado !== null){
+  get display(): string {
+    if (this.resultado !== null) {
+      this.hist.push(this.resultado.toString());
       return this.resultado.toString();
     }
-    if (this.numero2 !== null){
+    if (this.numero2 !== null) {
       return this.numero2;
     }
+
     return this.numero1;
+  }
+
+  limpar(): void {
+    this.numero1 = '0';
+    this.numero2 = null;
+    this.resultado = null;
+    this.operacao = null;
+    this.hist.forEach(() => {
+      this.hist = [''];
+    });
   }
 }
--- src/app/calculadora/services/calculadora.service.spec.ts
+++ src/app/calculadora/services/calculadora.service.spec.ts
@@ -14,11 +14,35 @@ describe('CalculadoraService', () => {
     expect(service).toBeTruthy();
   });
 
-  it('Deve garantir que 2 + 3 = 5',
-    inject([CalculadoraService], (service: CalculadoraService)=> {
+  it('Deve garantir que 2 + 3 = 5', inject(
+    [CalculadoraService],
+    (service: CalculadoraService) => {
       let soma = service.calcular(2, 3, CalculadoraService.SOMA);
       expect(soma).toEqual(5);
-    })
-  )
+    }
+  ));
 
+  it('Deve garantir que 2 - 3 = 1', inject(
+    [CalculadoraService],
+    (service: CalculadoraService) => {
+      let SUBTRACAO = service.calcular(2, 3, CalculadoraService.SUBTRACAO);
+      expect(SUBTRACAO).toEqual(-1);
+    }
+  ));
+
+  it('Deve garantir que 2 * 3 = 6', inject(
+    [CalculadoraService],
+    (service: CalculadoraService) => {
+      let mult = service.calcular(2, 3, CalculadoraService.MULTIPLICACAO);
+      expect(mult).toEqual(6);
+    }
+  ));
+
+  it('Deve garantir que 5 / 5 = 1', inject(
+    [CalculadoraService],
+    (service: CalculadoraService) => {
+      let DIVISAO = service.calcular(5, 5, CalculadoraService.DIVISAO);
+      expect(DIVISAO).toEqual(1);
+    }
+  ));
 });
